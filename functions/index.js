1ed43d64a60ba044ed339a74e4e5c979a2a3891b
Kevin Dyer <kevin.m.dyer@gmail.com>
2017-09-10 12:22:39 -0600

    Added several UI components.

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,7 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
-exports.checkTemperature = functions.database.ref('/temperature/{deviceId}')
+exports.checkTemperature = functions.database.ref('/devices/{deviceId}/temperature')
   .onWrite((event) => {
     const temperature = event.data.val();
     console.log(`The temperature is ${temperature}.`);
