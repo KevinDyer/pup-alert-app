ada1a31e1418717ab7964caf5df04c291456a783
Kevin Dyer <kevin.m.dyer@gmail.com>
2018-02-18 11:37:30 -0700

    feat: Added sensor update function

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,64 @@
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
-admin.initializeApp(functions.config().firebase);
+(() => {
+  'use strict';
 
-exports.checkTemperature = functions.database.ref('/devices/{deviceId}/temperature')
-  .onWrite((event) => {
-    const temperature = event.data.val();
-    console.log(`The temperature is ${temperature}.`);
+  const functions = require('firebase-functions');
+  const admin = require('firebase-admin');
+  const http = require('http');
+  const url = require('url');
+
+  admin.initializeApp(functions.config().firebase);
+
+  exports.updateSensor = functions.database.ref('/sessions/{sessionId}/environment/data/{timestamp}').onWrite((event) => {
+    const sessionId = event.params.sessionId;
+    const sensorData = event.data.val();
+    console.log(`Session[${sessionId}]: sensor data`, sensorData);
+    const {temperature} = sensorData;
+    const body = {
+      temperature: {
+        sensor_id: 'test-kevin',
+        value: temperature,
+        battery_level: 42,
+        is_charging: 1,
+      },
+    };
+    return sendSensorUpdate(body);
   });
+
+  function sendSensorUpdate(body) {
+    return new Promise((resolve, reject) => {
+      const bodyBuffer = Buffer.from(JSON.stringify(body));
+      const {hostname, path} = url.parse(functions.config().backend.url);
+      const options = Object.assign({}, {
+        method: 'POST',
+        hostname: hostname,
+        path: path,
+        headers: {
+          'Content-Type': 'application/json',
+          'Content-Length': bodyBuffer.length,
+        }
+      });
+      const req = http.request(options, (res) => {
+        res.once('error', reject);
+
+        const chunks = [];
+        res.on('data', (chunk) => chunks.push(chunk));
+
+        res.on('end', () => {
+          const body = Buffer.concat(chunks);
+          const {status, message} = JSON.parse(body.toString());
+          if (200 === res.statusCode && 1 === status) {
+            console.log(`Success: ${message}`);
+            resolve();
+          } else {
+            console.log(`Failed: ${message}`);
+            reject(new Error(message));
+          }
+        });
+      });
+      req.once('error', reject);
+      req.write(bodyBuffer);
+      req.end();
+    });
+  }
+
+})();
